b2544df94102c49c81c5be2ed450959d22983af8
Daniella Achoki <daniellaachoki@gmail.com>
2026-09-09 18:42:59 +0300

    Implement Book and Coffee classes with required properties and validation

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,22 @@
-#!/usr/bin/env python3
-
 class Coffee:
-    pass
+    def __init__(self, size, price):
+        self.set_size(size)
+        self.price = price
+
+    def get_size(self):
+        return self._size
+
+    def set_size(self, value):
+        valid_sizes = ["Small", "Medium", "Large"]
+        if value not in valid_sizes:
+            print("size must be Small, Medium, or Large")
+            self._size = None
+        else:
+            self._size = value
+
+    size = property(get_size, set_size)
+
+    def tip(self):
+        print("This coffee is great, here’s a tip!")
+        self.price += 1
+
